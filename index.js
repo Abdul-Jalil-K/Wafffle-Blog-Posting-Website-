717e301beb7a701f859e49020d1f4f2e267d61da
Abdul-Jalil Khalifah <abduljalilibnismail2@gmail.com>
2026-06-01 07:46:53 +0300

    Fix Typo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import env from "dotenv";
 env.config();
 
 const app = express();
-const port = rocess.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const HijriDate = hijriDate.default || hijriDate;
 
 app.use(bodyParser.urlencoded({ extended: true }));
